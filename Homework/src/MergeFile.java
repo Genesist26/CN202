4ad18aa2024e4ab92a51254251ebf3f970a07652
user <user@192.168.59.60>
2016-08-28 20:39:33 +0700

    Complete Homework 2.2 "MergeFile.java"

--- Homework/src/MergeFile.java
+++ Homework/src/MergeFile.java
@@ -3,6 +3,7 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class MergeFile {
@@ -15,11 +16,51 @@ public class MergeFile {
         Scanner sc1 = new Scanner(new FileReader(args[0]));
         Scanner sc2 = new Scanner(new FileReader(args[1]));
         PrintWriter out = new PrintWriter(new FileWriter(args[2]));
-
+        
+        
         try {
+            String allString = "";
+            while(sc1.hasNextLine())
+            {
+                allString += sc1.nextLine();
+                if(sc1.hasNextLine())
+                    allString += " ";
+                else
+                    break;
+                
+            }
+            allString += " ";
+            while(sc2.hasNextLine())
+            {
+                allString += sc2.nextLine();
+                if(sc2.hasNextLine())
+                    allString += " ";
+                else
+                    break;
+            }
+            String[] split = allString.split(" ");
+            int[] number = new int[split.length];
             
+            for(int i = 0; i < split.length ; i++)
+            {
+                number[i] = Integer.parseInt(split[i]);
+                
+            }
             
+            Arrays.sort(number);
             
+            allString = "";
+            
+            for(int i = 0; i < number.length ; i++)
+            {
+                String x = Integer.toString(number[i]);
+                allString += x;
+                if(i < number.length)
+                    allString += " ";
+                
+            }
+            
+            out.print(allString);
             
         } finally {
             sc1.close();
