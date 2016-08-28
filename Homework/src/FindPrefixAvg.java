de3351ccab979de2449d27b26b9735c35d4184ac
user <user@192.168.59.60>
2016-08-28 21:37:58 +0700

    Complete Homework 2.3 "FindPrefixAvg.java" v.0

---
+++ Homework/src/FindPrefixAvg.java
@@ -0,0 +1,51 @@
+
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class FindPrefixAvg {
+
+    public static void main(String[] args) throws IOException {
+
+        if (args.length != 2) {
+            usage();
+        }
+
+        Scanner sc = new Scanner(new FileReader(args[0]));
+        PrintWriter out = new PrintWriter(new FileWriter(args[1]));
+        try {
+            ArrayList<Double> list = new ArrayList<Double>();
+
+            while (sc.hasNextLine()) {
+                String line = sc.nextLine();
+                Scanner lineScanner = new Scanner(line);
+                while (lineScanner.hasNextDouble()) {
+                    double x = lineScanner.nextDouble();
+                    list.add(x);
+                }
+            }
+
+            double sum = 0;
+            int count = 1;
+            double prefix = 0;
+
+            for (int i = 0; i < list.size(); i++) {
+                sum += list.get(i);
+                prefix = sum / count++;
+                System.out.println(prefix);
+            }
+        } finally {
+            sc.close();
+            out.close();
+        }
+
+    }
+
+    public static void usage() {
+        System.out.println("Usage: java FindPrefixAvg inputTextFile OutputTextFile");
+        System.exit(1);
+    }
+}
