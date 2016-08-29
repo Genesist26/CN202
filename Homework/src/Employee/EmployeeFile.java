74f08add50e35e87f07d87468559ce07c35cebe1
user <user@192.168.59.60>
2016-08-29 18:51:57 +0700

    Complete Homework 2.5 "EmployeeFile.java"

--- Homework/src/Employee/EmployeeFile.java
+++ Homework/src/Employee/EmployeeFile.java
@@ -1,22 +1,60 @@
 package Employee;
 
 import java.io.*;
+import java.util.ArrayList;
 
 public class EmployeeFile {
 
     public static void addEmployee(Employee e, String employeeFileName)
             throws IOException, ClassNotFoundException {
+
+        File file = new File(employeeFileName);
+
+        if (!file.exists()) {
+            ArrayList<Employee> list = new ArrayList<Employee>();
+            list.add(e);
+
+            FileOutputStream out = new FileOutputStream(employeeFileName);
+            ObjectOutputStream outObj = new ObjectOutputStream(out);
+            outObj.writeObject(list);
+            outObj.close();
+        } else {
+
+            FileInputStream in = new FileInputStream(employeeFileName);
+            ObjectInputStream inObj = new ObjectInputStream(in);
+            ArrayList<Employee> list = (ArrayList<Employee>) inObj.readObject();
+            list.add(e);
+            inObj.close();
+
+            FileOutputStream out = new FileOutputStream(employeeFileName);
+            ObjectOutputStream outObj = new ObjectOutputStream(out);
+            outObj.writeObject(list);
+            outObj.close();
+
+        }
+
     }
 
     public static boolean print(String employeeFileName)
             throws IOException, ClassNotFoundException {
         File file = new File(employeeFileName);
-        
-        if(!file.exists())
+
+        if (!file.exists()) {
             return false;
+        }
+        
+        FileInputStream in = new FileInputStream(employeeFileName);
+        ObjectInputStream inObj = new ObjectInputStream(in);
+        ArrayList<Employee> list = new ArrayList<Employee>();
+        list = (ArrayList<Employee>) inObj.readObject();
+        
+        for(Employee e : list)
+            System.out.println(e);
+        
+        inObj.close();
         
         return true;
-        
+
     }
 
     public static boolean sort(String employeeFileName)
@@ -24,10 +62,19 @@ public class EmployeeFile {
         return true;
     }
 
-    public static void main(String[] args) throws IOException, ClassNotFoundException{
-        Employee e = new Employee("001", "A", "B", 10000.0);
-        
-        boolean b = EmployeeFile.print("employee.dat");
-        System.out.println(b);
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+        Employee e1 = new Employee("001", "A", "B", 10000.0);
+        Employee e2 = new Employee("002", "C", "D", 5000.0);
+
+        try {
+            EmployeeFile.addEmployee(e1, "employee.dat");
+            EmployeeFile.addEmployee(e2, "employee.dat");
+            
+            boolean b = EmployeeFile.print("employee.dat");
+            
+        } catch (Exception ex) {
+            System.out.println("Catched");
+        }
+
     }
 }
