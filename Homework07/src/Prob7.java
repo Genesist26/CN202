da7286c3477ec464949f6722fc4d7a6b1d7fd3cf
user <user@192.168.59.195>
2016-10-24 12:56:12 +0700

    Prob7.java Homework7

--- Homework07/src/Prob7.java
+++ Homework07/src/Prob7.java
@@ -1,51 +1,41 @@
 
 public class Prob7 {
+
     public static void main(String[] args) {
-        int[][] A = {{0,0,0,0}, {0,0,0,0}, {0,0,0,0}, {0,0,0,0}};
+        int[][] A = {{1, 0, 0, 0}, {1, 1, 1, 0}, {1, 1, 0, 0}, {1, 0, 0, 0}};
         System.out.println(countOne(A));
     }
-    public static int countOne(int[][] A){
+
+    public static int countOne(int[][] A) {
         int one = 0;
         int low;
         int high;
         int mid = 0;
-        
-        for(int i=0; i<A.length; i++){
+
+        for (int i = 0; i < A.length; i++) {
             low = 0;
-            high = A.length-1;
-            
-            while(low <= high){
-                mid = (low+high)/2;
-                
-                if(low == high){
-                    if(A[i][mid] == 0)
-                        System.out.println("0");
-                    else if(A[i][mid] == 1)
-                        System.out.println("1");
+            high = A.length - 1;
+
+            while (low <= high) {
+                mid = (low + high) / 2;
+
+                if ((low == high) && (high == mid) && (A[i][mid] == 0)) {
+                    one += mid;
                     break;
-                    
-                }
-                else if(A[i][mid] == 0)
+                } else if ((low == mid) && (A[i][mid] == 0)) {
+                    one += mid;
+                    break;
+                } else if ((low == high) && (high == mid) && (A[i][mid] == 1)) {
+                    one += mid + 1;
+                    break;
+                } else if (A[i][mid] == 0) {
                     high = mid - 1;
-                else if(A[i][mid] == 1){
+                } else if (A[i][mid] == 1) {
                     low = mid + 1;
                 }
-                
+
             }
         }
         return one;
     }
 }
-/*
-                if((low == mid) & A[i][mid] == 0)
-                    one += mid;
-                
-                else if(A[i][mid] == 0)
-                    high = mid - 1; 
-                else if(A[i][mid] == 1)
-                    low = mid + 1;
-                else if((low == high) && A[i][mid] == 0)
-                    one += mid;
-                else if((low == high) && A[i][mid] == 1)
-                    one += low +1;
-*/
