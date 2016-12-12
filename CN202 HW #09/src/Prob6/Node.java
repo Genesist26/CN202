3ba49d260bed56ef0854639a3988d9d6d0b4a9b8
somsa <somsa@DESKTOP-P6CPI48>
2016-12-12 14:21:34 +0700

    prob 6 complete

--- CN202 HW #09/src/Prob6/Node.java
+++ CN202 HW #09/src/Prob6/Node.java
@@ -10,41 +10,19 @@ package Prob6;
  * @author somsa
  */
 public class Node {
-    // instance variables
-
-    private int item;
-    private int maxItem;
     private Node next;
-
-    // constructor
-    public Node(int anItem, int theMaxItem, Node nextValue) {
-        item = anItem;
-        maxItem = theMaxItem;
-        next = nextValue;
+    private int item;
+    
+    public Node(int i, Node n){
+        next =n ;
+        item = i;
     }
-
-    // get-set methods
-    public int getItem() {
+    
+    public int getItem(){
         return item;
     }
-
-    public int getMaxItem() {
-        return maxItem;
-    }
-
-    public Node getNext() {
+    
+    public Node getNext(){
         return next;
-    }
-
-    public void setItem(int anItem) {
-        item = anItem;
-    }
-
-    public void setMaxItem(int theMaxItem) {
-        maxItem = theMaxItem;
-    }
-
-    public void setNextItem(Node nextValue) {
-        next = nextValue;
     }
 }
--- CN202 HW #09/src/Prob6/Stack.java
+++ CN202 HW #09/src/Prob6/Stack.java
@@ -1,109 +1,55 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Prob6;
 
-/**
- *
- * @author somsa
- */
 public class Stack implements StackADT {
-    // instance variables
 
     private Node top;
-    private int numItem;
-
-    // constructor
-    public Stack() {
-        top = null;
-        numItem = 0;
-    }
-
-    // methods
-    public int size() {
-        return numItem;
-    }
-
-    public boolean isEmpty() {
-        return (numItem == 0);
-    }
-
-    public int top() throws StackEmptyException {
-        if (isEmpty()) {
-            throw new StackEmptyException("Stack is empty.");
-        }
-        return top.getItem();
-    }
+    private int nItems;
+    private int max;
 
     public void push(int item) {
-        int max;
-
-        if ((top == null)
-                || // first item on stack
-                (item > top.getMaxItem())) // item is maximum
-        {
-            max = item;
-        } else {
-            max = top.getMaxItem();
+        Node newNode = new Node(item, top);
+        if (isEmpty()) {
+            max = newNode.getItem();
         }
-        Node newNode = new Node(item, max, top);
+        if (item > max) {
+            max = item;
+        }
         top = newNode;
-        numItem++;
+        nItems++;
     }
 
     public int pop() throws StackEmptyException {
-        if (isEmpty()) {
-            throw new StackEmptyException("Stack is empty.");
-        }
-        int popItem = top.getItem();
-        top = top.getNext();
-        numItem--;
-        return popItem;
+        if(isEmpty())
+            throw new StackEmptyException("Stack is empty");
+        int temp = top.getItem();
+        if(size() == 1)
+            top = null;
+        else
+            top = top.getNext();
+        nItems--;
+        return temp;
     }
 
-    
-    public int max2() throws StackEmptyException {
-        if (isEmpty()) {
-            throw new StackEmptyException("Stack is empty");
-        }
+    public int size() {
+        return nItems;
+    }
 
-        int max = top();
-        Stack stack2 = new Stack();
-        int pop;
-        while (!isEmpty()) {
-            pop = pop();
-            if (pop > max) {
-                max = pop;
-            }
-            
-            stack2.push(pop);
-        }
-        while(!stack2.isEmpty()){
-            push(pop());
-        }
+    public boolean isEmpty() {
+        return nItems == 0;
+    }
+
+    public int top() throws StackEmptyException {
+        if(isEmpty())
+            throw new StackEmptyException("Stack is empty");
+
+        return top.getItem();
+    }
+    
+    public int max() throws StackEmptyException
+    {
+        if(isEmpty())
+            throw new StackEmptyException("Stack is empty");
         
         return max;
-    }
-     
-
-    public int max() throws StackEmptyException {
-        if (isEmpty()) {
-            throw new StackEmptyException("Stack is empty.");
-        }
-        return top.getMaxItem();
-    }
-
-    public void printStack() {
-        System.out.println("Items on stack");
-        Node current = top;
-        while (current != null) {
-            System.out.println("item = " + current.getItem());
-            System.out.println("max = " + current.getMaxItem());
-            System.out.println();
-
-            current = current.getNext();
-        }
     }
 }
